014f9ef28d41b09a552128607bcdb0582a3d2d0a
Arturs Drozdovs <arturs.drozdovs@teampicnic.com>
2025-02-19 19:05:04 +0100

    Remove X-centric screen scripts

--- i3/.config/i3/3scr-work.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output eDP-1-1 --off --output DP-1-1 --off --output HDMI-1-1 --off --output DP-1-2 --mode 1920x1080 --pos 0x0 --rotate left --output DP-1-3 --mode 1920x1080 --pos 1080x0 --rotate left --output DP-1-4 --mode 1920x1080 --pos 2160x420 --rotate normal
